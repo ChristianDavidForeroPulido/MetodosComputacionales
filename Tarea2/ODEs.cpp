965b0527db368acaa3377f544b7de9c5debf0be7
ChristianDavidForeroPulido <cd.forero@uniandes.edu.co>
2019-07-18 23:32:01 +0000

    Todos lo metodos quedaron perfectos, faltan hacer bien las graficas

--- Tarea2/ODEs.cpp
+++ Tarea2/ODEs.cpp
@@ -13,16 +13,18 @@ double f_y(double , double , double, double, double);
 
 double G;
 double M_sol;
+double M_tierra;
 
 void eulerSegOrden(F, F, F, F, double, double, double, double, double);
 void leapFrogSegOrden(F, F, F, F, double, double, double, double, double);
+void rungeKuttaSegOrden(F, F, F, F, double, double, double, double, double);
 
-    
 int main()
 {
     //En masas solares, anios, y unidades astronomicas
     G = 39.478;
     M_sol = 1.0;
+    M_tierra = 3.00273*pow(10,-6);
     
     double vx0 = -6.35;
     double vy0 = 0.606;
@@ -32,6 +34,14 @@ int main()
     eulerSegOrden(f_vx, f_vy, f_x, f_y, x0, y0, vx0, vy0, 0.05);
     eulerSegOrden(f_vx, f_vy, f_x, f_y, x0, y0, vx0, vy0, 0.01);
     eulerSegOrden(f_vx, f_vy, f_x, f_y, x0, y0, vx0, vy0, 0.001);
+    
+    leapFrogSegOrden(f_vx, f_vy, f_x, f_y, x0, y0, vx0, vy0, 0.05);
+    leapFrogSegOrden(f_vx, f_vy, f_x, f_y, x0, y0, vx0, vy0, 0.01);
+    leapFrogSegOrden(f_vx, f_vy, f_x, f_y, x0, y0, vx0, vy0, 0.001);
+    
+    rungeKuttaSegOrden(f_vx, f_vy, f_x, f_y, x0, y0, vx0, vy0, 0.05);
+    rungeKuttaSegOrden(f_vx, f_vy, f_x, f_y, x0, y0, vx0, vy0, 0.01);
+    rungeKuttaSegOrden(f_vx, f_vy, f_x, f_y, x0, y0, vx0, vy0, 0.001);
     
     return 0;
 }
@@ -68,12 +78,15 @@ void eulerSegOrden(F funVx, F funVy, F funX, F funY, double x_0, double y_0, dou
     double v_xPas = vx_0;
     double v_yPas = vy_0;
     
+    double energia = (0.5*M_tierra*(pow(v_x,2) + pow(v_y,2))) - (G*M_sol*M_tierra+pow(pow(x,2) + pow(y,2), -0.5));
+    double momento = M_tierra*((x*v_y) - (y*v_x));
+    
     ofstream outfile;
     outfile.open("euler_dt="+to_string(dt)+".dat");
     
     for(double t = 0.0; t < 20.0; t+=dt)
     {
-        outfile<<t<<" "<<x<<" "<<y<<" "<<v_x<<" "<<v_y<<endl;
+        outfile<<t<<" "<<x<<" "<<y<<" "<<energia<<" "<<momento<<endl;
         
         xPas = x;
         yPas = y;
@@ -84,6 +97,9 @@ void eulerSegOrden(F funVx, F funVy, F funX, F funY, double x_0, double y_0, dou
         v_y += dt*funY(t, xPas, yPas, v_xPas, v_yPas);
         x += dt*funVx(t, xPas, yPas, v_xPas, v_yPas);
         y += dt*funVy(t, xPas, yPas, v_xPas, v_yPas);
+        
+        energia = (0.5*M_tierra*(pow(v_x,2) + pow(v_y,2))) - (G*M_sol*M_tierra+pow(pow(x,2) + pow(y,2), -0.5));
+        momento = M_tierra*((x*v_y) - (y*v_x));
     }
     
     outfile.close();
@@ -96,27 +112,122 @@ void leapFrogSegOrden(F funVx, F funVy, F funX, F funY, double x_0, double y_0, 
     double v_x = vx_0;
     double v_y = vy_0;
     
+    double xPre = x_0;
+    double yPre = y_0;
+    double v_xPre = vx_0;
+    double v_yPre = vy_0;
+    
+    double v_xPas = v_x - dt*funX(0.0, xPre, yPre, v_xPre, v_yPre);
+    double v_yPas = v_y - dt*funY(0.0, xPre, yPre, v_xPre, v_yPre);
+    double xPas = x - dt*funVx(0.0, xPre, yPre, v_xPre, v_yPre);
+    double yPas = y - dt*funVy(0.0, xPre, yPre, v_xPre, v_yPre);
+    
+    double energia = (0.5*M_tierra*(pow(v_x,2) + pow(v_y,2))) - (G*M_sol*M_tierra+pow(pow(x,2) + pow(y,2), -0.5));
+    double momento = M_tierra*((x*v_y) - (y*v_x));
+    
+    ofstream outfile;
+    outfile.open("leapFrog_dt="+to_string(dt)+".dat");
+    
+    for(double t = 0.0; t < 20.0; t+=dt)
+    {
+        outfile<<t<<" "<<x<<" "<<y<<" "<<energia<<" "<<momento<<endl;
+        
+        xPas = xPre;
+        yPas = yPre;
+        v_xPas = v_xPre;
+        v_yPas = v_yPre;
+        
+        xPre = x;
+        yPre = y;
+        v_xPre = v_x;
+        v_yPre = v_y;
+        
+        v_x = v_xPas + 2*dt*funX(t, xPre, yPre, v_xPre, v_yPre);
+        v_y = v_yPas + 2*dt*funY(t, xPre, yPre, v_xPre, v_yPre);
+        x = xPas + 2*dt*funVx(t, xPre, yPre, v_xPre, v_yPre);
+        y = yPas + 2*dt*funVy(t, xPre, yPre, v_xPre, v_yPre);
+        
+        energia = (0.5*M_tierra*(pow(v_x,2) + pow(v_y,2))) - (G*M_sol*M_tierra+pow(pow(x,2) + pow(y,2), -0.5));
+        momento = M_tierra*((x*v_y) - (y*v_x));
+    }
+    
+    outfile.close();
+}
+
+void rungeKuttaSegOrden(F funVx, F funVy, F funX, F funY, double x_0, double y_0, double vx_0, double vy_0, double dt)
+{
+    double x = x_0;
+    double y = y_0;
+    double v_x = vx_0;
+    double v_y = vy_0;
+    
     double xPas = x_0;
     double yPas = y_0;
     double v_xPas = vx_0;
     double v_yPas = vy_0;
     
+    double vxK1;
+    double vyK1;
+    double xK1;
+    double yK1;
+    
+    double vxK2;
+    double vyK2;
+    double xK2;
+    double yK2;
+    
+    double vxK3;
+    double vyK3;
+    double xK3;
+    double yK3;
+    
+    double vxK4;
+    double vyK4;
+    double xK4;
+    double yK4;
+    
+    double energia = (0.5*M_tierra*(pow(v_x,2) + pow(v_y,2))) - (G*M_sol*M_tierra+pow(pow(x,2) + pow(y,2), -0.5));
+    double momento = M_tierra*((x*v_y) - (y*v_x));
+    
     ofstream outfile;
-    outfile.open("euler_dt="+to_string(dt)+".dat");
+    outfile.open("rungeKutta_dt="+to_string(dt)+".dat");
     
     for(double t = 0.0; t < 20.0; t+=dt)
-    {
-        outfile<<t<<" "<<x<<" "<<y<<" "<<v_x<<" "<<v_y<<endl;
+    {        
+        outfile<<t<<" "<<x<<" "<<y<<" "<<energia<<" "<<momento<<endl;
         
         xPas = x;
         yPas = y;
         v_xPas = v_x;
         v_yPas = v_y;
         
-        v_x += dt*funX(t, xPas, yPas, v_xPas, v_yPas);
-        v_y += dt*funY(t, xPas, yPas, v_xPas, v_yPas);
-        x += dt*funVx(t, xPas, yPas, v_xPas, v_yPas);
-        y += dt*funVy(t, xPas, yPas, v_xPas, v_yPas);
+        vxK1 = dt*funX(t, xPas, yPas, v_xPas, v_yPas);
+        vyK1 = dt*funY(t, xPas, yPas, v_xPas, v_yPas);
+        xK1 = dt*funVx(t, xPas, yPas, v_xPas, v_yPas);
+        yK1 = dt*funVy(t, xPas, yPas, v_xPas, v_yPas);
+        
+        vxK2 = dt*funX(t + 0.5*dt, xPas + 0.5*xK1, yPas + 0.5*yK1, v_xPas + 0.5*vxK1, v_yPas + 0.5*vyK1);
+        vyK2 = dt*funY(t + 0.5*dt, xPas + 0.5*xK1, yPas + 0.5*yK1, v_xPas + 0.5*vxK1, v_yPas + 0.5*vyK1);
+        xK2 = dt*funVx(t + 0.5*dt, xPas + 0.5*xK1, yPas + 0.5*yK1, v_xPas + 0.5*vxK1, v_yPas + 0.5*vyK1);
+        yK2 = dt*funVy(t + 0.5*dt, xPas + 0.5*xK1, yPas + 0.5*yK1, v_xPas + 0.5*vxK1, v_yPas + 0.5*vyK1);
+        
+        vxK3 = dt*funX(t + 0.5*dt, xPas + 0.5*xK2, yPas + 0.5*yK2, v_xPas + 0.5*vxK2, v_yPas + 0.5*vyK2);
+        vyK3 = dt*funY(t + 0.5*dt, xPas + 0.5*xK2, yPas + 0.5*yK2, v_xPas + 0.5*vxK2, v_yPas + 0.5*vyK2);
+        xK3 = dt*funVx(t + 0.5*dt, xPas + 0.5*xK2, yPas + 0.5*yK2, v_xPas + 0.5*vxK2, v_yPas + 0.5*vyK2);
+        yK3 = dt*funVy(t + 0.5*dt, xPas + 0.5*xK2, yPas + 0.5*yK2, v_xPas + 0.5*vxK2, v_yPas + 0.5*vyK2);
+        
+        vxK4 = dt*funX(t + dt, xPas + xK3, yPas + yK3, v_xPas + vxK3, v_yPas + vyK3);
+        vyK4 = dt*funY(t + dt, xPas + xK3, yPas + yK3, v_xPas + vxK3, v_yPas + vyK3);
+        xK4 = dt*funVx(t + dt, xPas + xK3, yPas + yK3, v_xPas + vxK3, v_yPas + vyK3);
+        yK4 = dt*funVy(t + dt, xPas + xK3, yPas + yK3, v_xPas + vxK3, v_yPas + vyK3);
+        
+        v_x += (vxK1 + 2*vxK2 + 2*vxK3 + vxK4)/6;
+        v_y += (vyK1 + 2*vyK2 + 2*vyK3 + vyK4)/6;
+        x += (xK1 + 2*xK2 + 2*xK3 + xK4)/6;
+        y += (yK1 + 2*yK2 + 2*yK3 + yK4)/6;
+        
+        energia = (0.5*M_tierra*(pow(v_x,2) + pow(v_y,2))) - (G*M_sol*M_tierra+pow(pow(x,2) + pow(y,2), -0.5));
+        momento = M_tierra*((x*v_y) - (y*v_x));
     }
     
     outfile.close();
--- Tarea2/Plots_hw2.py
+++ Tarea2/Plots_hw2.py
@@ -5,14 +5,48 @@ euler1 = np.genfromtxt("euler_dt=0.050000.dat")
 euler2 = np.genfromtxt("euler_dt=0.010000.dat")
 euler3 = np.genfromtxt("euler_dt=0.001000.dat")
 
+leapFrog1 = np.genfromtxt("leapFrog_dt=0.050000.dat")
+leapFrog2 = np.genfromtxt("leapFrog_dt=0.010000.dat")
+leapFrog3 = np.genfromtxt("leapFrog_dt=0.001000.dat")
+
+rungeKutta1 = np.genfromtxt("rungeKutta_dt=0.050000.dat")
+rungeKutta2 = np.genfromtxt("rungeKutta_dt=0.010000.dat")
+rungeKutta3 = np.genfromtxt("rungeKutta_dt=0.001000.dat")
+
 plt.figure()
 plt.plot(euler1[:,1], euler1[:,2])
-plt.savefig("pruebaeuler1.png")
+plt.savefig("pruebaEuler1.png")
 
 plt.figure()
 plt.plot(euler2[:,1], euler2[:,2])
-plt.savefig("pruebaeuler2.png")
+plt.savefig("pruebaEuler2.png")
 
 plt.figure()
 plt.plot(euler3[:,1], euler3[:,2])
-plt.savefig("pruebaeuler3.png")
+plt.savefig("pruebaEuler3.png")
+
+plt.figure()
+plt.plot(leapFrog1[:,1], leapFrog1[:,2])
+plt.savefig("pruebaLeapFrog1.png")
+
+plt.figure()
+plt.plot(leapFrog2[:,1], leapFrog2[:,2])
+plt.savefig("pruebaLeapFrog2.png")
+
+plt.figure()
+plt.plot(leapFrog3[:,1], leapFrog3[:,2])
+plt.savefig("pruebaLeapFrog3.png")
+
+
+plt.figure()
+plt.plot(rungeKutta1[:,1], rungeKutta1[:,2])
+plt.savefig("pruebaRungeKutta1.png")
+
+plt.figure()
+plt.plot(rungeKutta2[:,1], rungeKutta2[:,2])
+plt.savefig("pruebaRungeKutta2.png")
+
+plt.figure()
+plt.plot(rungeKutta3[:,1], rungeKutta3[:,2])
+plt.savefig("pruebaRungeKutta3.png")
+
